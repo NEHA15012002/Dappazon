a5df85dbc2f8c14da6cab0d1a740acfc288a676f
NEHA15012002 <72308843+NEHA15012002@users.noreply.github.com>
2023-07-17 20:31:12 +0530

    first commit

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,122 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+//Smart Contract for the Amazon for blockchain - Dappazon
+// Name: Neha Agarwal
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+    //for setting the items in the blockchain we need mapping
+    //mapping will help me treat blockchain as a database for key-value pair
+    mapping(uint256 => Item) public items;
+    mapping(address => mapping(uint256 => Order)) public orders;
+    mapping(address => uint256) public orderCount;
+
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+    event List(string name, uint256 cost, uint256 quantity);
+
+
+
+    //we can modify the behaviour of the functions by using modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    //setting the value of the owner by passing it in the constructor
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    //1. list products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        // Create Item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Add Item to mapping
+        items[_id] = item;
+
+        // Emit event
+        emit List(_name, _cost, _stock);
+    }
+
+    //2. buy item
+    function buy(uint256 _id) public payable {
+        
+        
+        //recieve cryto
+        // Fetch item
+        Item memory item = items[_id];
+
+        // Require enough ether to buy item
+        require(msg.value >= item.cost);
+
+        // Require item is in stock
+        require(item.stock > 0);
+
+        // Create order
+        Order memory order = Order(block.timestamp, item);
+
+        // Add order for user
+        orderCount[msg.sender]++; // <-- Order ID
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Subtract stock
+        items[_id].stock = item.stock - 1;
+
+        // Emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+    //3. Withdraw Funds
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
